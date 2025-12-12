19bef0170b03d860d263ded428f78478d40528b1
jaekw <jae-kwen@daum.net>
2025-12-12 17:08:50 +0900

    feature : client, CORS, circuitbreaker(일부) 추가

---
+++ client/src/test/java/com/jstudy/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstudy.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- gateway/src/main/java/com/jstudy/gateway/FallBackController.java
+++ gateway/src/main/java/com/jstudy/gateway/FallBackController.java
@@ -1,8 +1,6 @@
 package com.jstudy.gateway;
 
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
